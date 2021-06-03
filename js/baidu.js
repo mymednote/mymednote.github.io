ecc30a6fe0eb7e3cf989d5f45da1cf6544d7e1e9
mymednote <81005699+mymednote@users.noreply.github.com>
2021-06-03 21:15:57 +0800

    deploy

---
+++ js/baidu.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?a21449016f5def871ac321ab56481a20";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
